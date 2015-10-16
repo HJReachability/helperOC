b644ba2e266147395bb071a24cee8e367244e7f8
Mo Chen <mochen72@berkeley.edu>
2015-10-16 10:42:00 -0700

    Redid air3D2csv.m

--- air3D2csv.m
+++ air3D2csv.m
@@ -1,47 +1,18 @@
-function data2D = air3D2csv(g, data3D, output_file, header)
-% function data2D = air3D2csv(g, data3D, output_file, header)
-%
-% Converts a 3D array from the air3D output into a 2D array which is then
-% exported as a csv file. Each row in the csv file corresponds to
-% data3D(i,j,:) (i.e. the relative x and y positions), and each column
-% represents a relative heading.
-%
-% Inputs: g           - grid structure
-%         data3D      - original 3D array from air3D.m output
-%         output_file - output file name
-%         header      - set to 1 to enable header rows (1) and columns (2)
-% Output: data2D      - resulting 2D array (which is saved as csv)
-%
-% Mo Chen, 2015-10-12
+function dataOut = air3D2csv(values, gradients, filename)
 
-% Index offsets if using header
-if header
-  roffset = 1;
-  coffset = 2;
-else
-  roffset = 0;
-  coffset = 0;
-end
+dataOut = zeros(numel(values), 4);
 
-% Initialize output data, adding in row header if needed
-data2D = zeros(g.N(1)*g.N(2)+roffset, g.N(3)+coffset);
-if header
-  data2D(1, 1+coffset:end) = g.vs{3}';
-end
+dataOut(:,1) = values(:);
 
-% Fill in the rest of the rows, adding column headers if needed
-for i = 1:g.N(1)
-  for j = 1:g.N(2)
-    if header
-      data2D((i-1)*g.N(2)+j+roffset, 1) = g.vs{1}(i);
-      data2D((i-1)*g.N(2)+j+roffset, 2) = g.vs{2}(j);
-    end
-
-    data2D((i-1)*g.N(2)+j+roffset, 1+coffset:end) = ...
-      squeeze(data3D(i,j,:))';
+for i = 1:3
+  if numel(gradients{i}) ~= numel(values)
+    error(['Number of elements in gradient component ' num2str(i) ...
+      ' does not match number of elements in value function!'])
   end
+  
+  dataOut(:,i+1) = gradients{i}(:);
 end
 
-% Output csv file
-csvwrite(output_file, data2D);
+csvwrite(filename, dataOut);
+
 end
