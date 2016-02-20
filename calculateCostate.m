688c2a447db96d21383d02fbf109c881153fa018
Mo Chen <mochen72@berkeley.edu>
2016-02-20 12:18:54 -0800

    Changed eval_u to properly deal with periodic boundary conditions in any dimension; changed calculateCostate to call eval_u.

--- calculateCostate.m
+++ calculateCostate.m
@@ -2,7 +2,7 @@ function p = calculateCostate(g, P, x)
 % function p = calculateCostate(g, P, x)
 %
 % Calculates the gradient (costate) at x given an array of gradients stored
-% in P. Periodicity is automatically checked
+% in P. Periodicity is automatically checked in eval_u
 %
 % Inputs: 
 %   g - grid structure
@@ -13,172 +13,14 @@ function p = calculateCostate(g, P, x)
 %   p - interpolated gradient at x
 %
 % Mo Chen, 2015-10-15
-% Originally adapted from Haomiao Huang's code
+% Updated 2016-02-20
 
 % Check input
-x = checkInterpInput(g, x);
-
 p = zeros(size(x,1), g.dim);
 
-switch g.dim
-  case 1
-    % Dealing with periodicity
-    if isequal(g.bdry, @addGhostPeriodic)
-      g.vs{1} = cat(1, g.vs{1}, g.vs{1}(end)+g.dx(1));
-      P{1} = cat(1, P{1}, P{1}(1));
-    end
-    
-    % Interpolate
-    p(:,1) = interpn(g.vs{1}, P{1}, x(:,1));
-    
-  case 2
-    % Dealing with periodicity
-    for i = 1:2
-      if isequal(g.bdry{i}, @addGhostPeriodic)
-        g.vs{i} = cat(1, g.vs{i}, g.vs{i}(end) + g.dx(i));
-      end
-    end
-    
-    if isequal(g.bdry{1}, @addGhostPeriodic)
-      for i = 1:2
-        P{i} = cat(1, P{i}, P{i}(1,:));
-      end
-    end
-    
-    if isequal(g.bdry{2}, @addGhostPeriodic)
-      for i = 1:2
-        P{2} = cat(2, P{i}, P{i}(:,1));
-      end
-    end
-    
-    % Interpolate
-    for i = 1:2
-      p(:,i) = interpn(g.vs{1}, g.vs{2}, P{i}, x(:,1), x(:,2));
-    end
-    
-  case 3
-    % Dealing with periodicity
-    for i = 1:3
-      if isequal(g.bdry{i}, @addGhostPeriodic)
-        g.vs{i} = cat(1, g.vs{i}, g.vs{i}(end) + g.dx(i));
-      end
-    end
-    
-    if isequal(g.bdry{1}, @addGhostPeriodic)
-      for i = 1:3
-        P{1} = cat(1, P{i}, P{i}(i,:,:));
-      end
-    end
-    
-    if isequal(g.bdry{2}, @addGhostPeriodic)
-      for i = 1:3
-        P{2} = cat(2, P{i}, P{i}(:,1,:));
-      end
-    end
-    
-    if isequal(g.bdry{3}, @addGhostPeriodic)
-      for i = 1:3
-        P{i} = cat(3, P{i}, P{i}(:,:,1));
-      end
-    end
-    
-    % Interpolate
-    for i = 1:3
-      p(:,i) = interpn(g.vs{1}, g.vs{2}, g.vs{3}, P{i}, x(:,1), ...
-        x(:,2), x(:,3));
-    end
-    
-  case 4
-    % Dealing with periodicity
-    for i = 1:4
-      if isequal(g.bdry{i}, @addGhostPeriodic)
-        g.vs{i} = cat(1, g.vs{i}, g.vs{i}(end) + g.dx(i));
-      end
-    end
-    
-    if isequal(g.bdry{1}, @addGhostPeriodic)
-      for i = 1:4
-        P{i} = cat(1, P{i}, P{i}(1,:,:,:));
-      end
-    end
-    
-    if isequal(g.bdry{2}, @addGhostPeriodic)
-      for i = 1:4
-        P{i} = cat(2, P{i}, P{i}(:,1,:,:));
-      end
-    end
-    
-    if isequal(g.bdry{3}, @addGhostPeriodic)
-      for i = 1:4
-        P{i} = cat(3, P{i}, P{i}(:,:,1,:));
-      end
-    end
-    
-    if isequal(g.bdry{4}, @addGhostPeriodic)
-      for i = 1:4
-        P{4} = cat(4, P{i}, P{i}(:,:,:,1));
-      end
-    end
-    
-    % Interpolate
-    for i = 1:4
-      p(:,i) = interpn(g.vs{1}, g.vs{2}, g.vs{3}, g.vs{4}, P{i}, ... 
-        x(:,1), x(:,2), x(:,3), x(:,4));
-    end
-    
-  case 6
-    % Dealing with periodicity
-    for i = 1:g.dim
-      if isequal(g.bdry{i}, @addGhostPeriodic)
-        g.vs{i} = cat(1, g.vs{i}, g.vs{i}(end) + g.dx(i));
-      end
-    end
-    
-    if isequal(g.bdry{1}, @addGhostPeriodic)
-      for i = 1:6
-        P{i} = cat(1, P{i}, P{i}(1,:,:,:,:,:));
-      end
-    end
-    
-    if isequal(g.bdry{2}, @addGhostPeriodic)
-      for i = 1:6
-        P{i} = cat(2, P{i}, P{i}(:,1,:,:,:,:));
-      end
-    end
-    
-    if isequal(g.bdry{3}, @addGhostPeriodic)
-      for i = 1:6
-        P{i} = cat(3, P{i}, P{i}(:,:,1,:,:,:));
-      end
-    end
-    
-    if isequal(g.bdry{4}, @addGhostPeriodic)
-      for i = 1:6
-        P{i} = cat(4, P{i}, P{i}(:,:,:,1,:,:));
-      end
-    end
-    
-    if isequal(g.bdry{5}, @addGhostPeriodic)
-      for i = 1:6
-        P{i} = cat(5, P{i}, P{i}(:,:,:,:,1,:));
-      end
-    end
-    
-    if isequal(g.bdry{6}, @addGhostPeriodic)
-      for i = 1:6
-        P{i} = cat(6, P{i}, P{i}(:,:,:,:,:,1));
-      end
-    end
-    
-    % Interpolate
-    for i = 1:g.dim
-      p(:,i) = interpn(g.vs{1}, g.vs{2}, g.vs{3}, g.vs{4}, g.vs{5}, ...
-        g.vs{6}, P{i}, x(:,1), x(:,2), x(:,3), x(:,4), x(:,5), x(:,6));
-    end
-    
-  otherwise
-    error(['calculateCostate has not been implemented for ' ... 
-      num2str(g.dim) ' dimensions!'])
+% Interpolate gradient
+for i = 1:g.dim
+  p(i) = eval_u(g, P{i}, x);
 end
 
 end
--- eval_u.m
+++ eval_u.m
@@ -10,34 +10,73 @@ function v = eval_u(g, data, x)
 % OUTPUT
 %   v:  value at points x
 %
-% NOTE: For 3D arrays, this code automatically checks whether the 3rd
-% dimension is periodic by checking whether g.bdry{3} is @addGhostPeriodic
-%
 % Mo Chen, 2015-10-15
+% Updated 2016-02-19
 
+%% Dealing with periodicity
+for i = 1:g.dim
+  if isequal(g.bdry{i}, @addGhostPeriodic)
+    % Grid points
+    g.vs{i} = cat(1, g.vs{i}, g.vs{i}(end) + g.dx(i));
+
+    % Input data
+    eval(periodicAugmentCmd(i, g.dim));
+  end
+end
+
+%% Interpolate
 % Input checking
 x = checkInterpInput(g, x);
 
 switch g.dim
   case 1
     v = interpn(g.vs{1}, data, x);
+    
   case 2
     v = interpn(g.vs{1}, g.vs{2}, data, x(:,1),x(:,2));
+    
   case 3
-    if isequal(g.bdry{3}, @addGhostPeriodic)
-      v = interpn(g.vs{1}, g.vs{2}, [g.vs{3}; 2*pi], cat(3, data, ... 
-        data(:,:,1)), x(:,1),x(:,2),x(:,3));
-    else
-      v = interpn(g.vs{1}, g.vs{2}, g.vs{3}, data, x(:,1),x(:,2),x(:,3));
-    end
+    v = interpn(g.vs{1}, g.vs{2}, g.vs{3}, data, x(:,1),x(:,2),x(:,3));
+    
   case 4
     v = interpn(g.vs{1},g.vs{2},g.vs{3}, g.vs{4}, data, ...
       x(:,1),x(:,2),x(:,3),x(:,4));
+    
   case 6
     v = interpn(g.vs{1}, g.vs{2},g.vs{3}, g.vs{4}, g.vs{5}, g.vs{6}, ...
       data, x(:,1), x(:,2), x(:,3), x(:,4), x(:,5), x(:,6));
+    
   otherwise
     error(['Cannot evaluate matrices with dimension' num2str(g.dim) '!'])
 end
 
+end
+
+function cmd = periodicAugmentCmd(idim, dims)
+% cmd = periodicAugmentCmd(idim, dims)
+%
+% Creates the command for concatenating the first slice of data to the end of
+% the data to deal with periodic dimensions.
+%
+% eg. periodicAugmentCmd(1, 3) returns 'cat(1, data, data(1,:,:))'
+%     periodicAugmentCmd(3, 3) returns 'cat(3, data, data(:,:,1))'
+%
+% Mo Chen, 2016-02-19
+
+
+cmd = ['data = cat(' num2str(idim) ', data, data('];
+
+for i = 1:dims
+  if i == idim
+    cmd = [cmd '1'];
+  else
+    cmd = [cmd ':'];
+  end
+  
+  if i == dims
+    cmd = [cmd '));'];
+  else
+    cmd = [cmd ','];
+  end
+end
 end
