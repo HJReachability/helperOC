cb47d4fbab8f2ded507abfdb59c0292cec6780c8
Mo Chen <mochen72@berkeley.edu>
2016-04-26 10:51:12 -0700

    Added comments to HJIPDE_solve_test

--- tests/HJIPDE_solve_test.m
+++ tests/HJIPDE_solve_test.m
@@ -1,38 +1,52 @@
 function HJIPDE_solve_test()
 % HJIPDE_solve_test()
+% 
+% Tests the HJIPDE_solve function as well as provide an example of how to
+% use it.
 %
 % Mo Chen, 2016-04-18
 
 %% Grid
-grid_min = [-5; -5; -pi];
-grid_max = [5; 5; pi];
-N = [41; 41; 41];
-pdDims = 3;
+grid_min = [-5; -5; -pi]; % Lower corner of computation domain
+grid_max = [5; 5; pi];    % Upper corner of computation domain
+N = [41; 41; 41];         % Number of grid points per dimension
+pdDims = 3;               % 3rd diemension is periodic
 g = createGrid(grid_min, grid_max, N, pdDims);
+% Use "g = createGrid(grid_min, grid_max, N);" if there are no periodic
+% state space dimensions
 
 %% target set
 R = 1;
 data0 = shapeCylinder(g, 3, [0; 0; 0], R);
+% also try shapeRectangleByCorners, shapeSphere, etc.
 
 %% time vector
 t0 = 0;
 tMax = 2;
 dt = 0.025;
 tau = t0:dt:tMax;
+% If intermediate results are not needed, use tau = [t0 tMax];
 
 %% problem parameters
 speed = 1;
 U = [-1 1];
 
 %% Pack problem parameters
-schemeData.grid = g;
+schemeData.grid = g; % Grid MUST be specified!
 schemeData.U = U;
 schemeData.speed = speed;
+
+% ----- System dynamics are specified here -----
 schemeData.hamFunc = @dubins3DHamFunc;
 schemeData.partialFunc = @dubins3DPartialFunc;
 
 %% Compute time-dependent value function
 minWiths = {'none', 'zero', 'data0'};
+% selecting 'zero' computes reachable tube (usually, choose this option)
+% selecting 'none' computes reachable set
+% selecting 'data0' computes reachable tube, but only use this if there are
+%   obstacles (constraint/avoid sets) in the state space
+
 numPlots = 4;
 spC = ceil(sqrt(numPlots));
 spR = ceil(numPlots / spC);
@@ -53,6 +67,12 @@ for i = 1:length(minWiths)
     drawnow
   end
 end
+
+% In practice, most of the time, the above for loop is not needed, and the
+% code below is also not needed. Simply select an minWith option, and then
+% also input obstacles if they are present.
+
+% Change visualization code as necessary
 
 %% Test using single obstacle
 obstacles = shapeCylinder(g, 3, [1.5; 1.5; 0], 0.75*R);
