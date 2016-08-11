85470dc392a546fc9003b64f2da53ec7f09001c0
Mo Chen <mochen72@berkeley.edu>
2016-08-10 20:11:34 -0700

    added plotDisk

---
+++ plotDisk.m
@@ -0,0 +1,16 @@
+function h = plotDisk(center, radius, varargin)
+% h = plotDisk(center, radius, varargin)
+%     Plots a disk of some radius around some center (useful for plotting 
+%     collision sets)
+
+if radius < 0
+  error('Radius must be non-negative!')
+end
+
+t = linspace(0, 2*pi, 100);
+x = radius*cos(t) + center(1);
+y = radius*sin(t) + center(2);
+
+h = plot(x, y, varargin{:});
+
+end
